b804f9f5717e884a908f6828e56f1680ef504fc7
akmalluthfi <akmalluthfi19@gmail.com>
2023-01-12 16:50:05 +0700

    menganti package laravel midtrans dengan php midtrans

---
+++ config/midtrans.php
@@ -0,0 +1,20 @@
+<?php
+// config for Sawirricardo/Midtrans/Laravel
+return [
+    'merchant_id' => env('MIDTRANS_MERCHAT_ID'),
+    'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY'),
+
+    'sandbox_server_key' => env('MIDTRANS_SB_SERVER_KEY'),
+    'sandbox_client_key' => env('MIDTRANS_SB_CLIENT_KEY'),
+
+    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+    'is_sanitized' => true,
+    'is_3ds' => true,
+
+    'append_notif_url' => null,
+    'overrideNotifUrl' => null,
+    'payment_idempotency_key' => null,
+
+    'curl_options' => null,
+];
