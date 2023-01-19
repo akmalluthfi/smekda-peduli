152491fc0afcf34d66f2be6fa1634695193d6390
akmalluthfi <akmalluthfi19@gmail.com>
2023-01-19 08:27:08 +0700

    mengubah tampilan navbar admin

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -12,24 +12,25 @@
 
       <li class="nav-item dropdown pe-3">
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <img src="/img/default.png" alt=" Profile" class="rounded-circle" />
-          <span class="d-none d-md-block dropdown-toggle ps-2">Akmal Luthfi</span>
+          <img src="{{ auth()->user()->picture ? asset('storage/' . auth()->user()->picture) : '/img/default.png' }}"
+            alt=" Profile" class="rounded-circle" />
+          <span class="d-none d-md-block dropdown-toggle ps-2">{{ auth()->user()->name }}</span>
         </a>
         {{-- End Profile Image Icon --}}
 
         <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
           <li class="dropdown-header">
-            <h6>Kevin Anderson</h6>
-            <span>Web Designer</span>
+            <h6>{{ auth()->user()->name }}</h6>
+            <span>Admin</span>
           </li>
           <li>
             <hr class="dropdown-divider" />
           </li>
 
           <li>
-            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-              <i class="bi bi-person"></i>
-              <span>My Profile</span>
+            <a class="dropdown-item d-flex align-items-center" href="/user">
+              <i class="bi bi-person me-2"></i>
+              <span>Profile Saya</span>
             </a>
           </li>
           <li>
@@ -37,9 +38,9 @@
           </li>
 
           <li>
-            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-              <i class="bi bi-gear"></i>
-              <span>Account Settings</span>
+            <a class="dropdown-item d-flex align-items-center" href="/">
+              <i class="bi bi-house me-2"></i>
+              <span>Home</span>
             </a>
           </li>
           <li>
@@ -47,9 +48,9 @@
           </li>
 
           <li>
-            <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-              <i class="bi bi-question-circle"></i>
-              <span>Need Help?</span>
+            <a class="dropdown-item d-flex align-items-center" href="/help">
+              <i class="bi bi-question-circle me-2"></i>
+              <span>Bantuan</span>
             </a>
           </li>
           <li>
@@ -57,10 +58,14 @@
           </li>
 
           <li>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <i class="bi bi-box-arrow-right"></i>
-              <span>Sign Out</span>
-            </a>
+            <form action="/logout" method="POST">
+              @method('delete')
+              @csrf
+              <button type="submit" class="dropdown-item d-flex align-items-center text-danger" href="/logout">
+                <i class="bi bi-box-arrow-right me-2"></i>
+                <span>Keluar</span>
+              </button>
+            </form>
           </li>
         </ul>
         {{-- End Profile Dropdown Items --}}
