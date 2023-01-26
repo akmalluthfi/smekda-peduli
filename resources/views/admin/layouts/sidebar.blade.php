52da108b928269620176e2f37d9ab1386558082f
akmalluthfi <akmalluthfi19@gmail.com>
2023-01-26 07:17:18 +0700

    menghapus view admin.payment-method

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,26 +1,19 @@
 <aside id="sidebar" class="sidebar">
-    <ul class="sidebar-nav" id="sidebar-nav">
+  <ul class="sidebar-nav" id="sidebar-nav">
 
-        <li class="nav-item">
-            <a class="nav-link {{ Request::is('admin') ? 'active' : '' }}" href="/admin">
-                <i class="bi bi-grid"></i>
-                <span>Dashboard</span>
-            </a>
-        </li>
+    <li class="nav-item">
+      <a class="nav-link {{ Request::is('admin') ? 'active' : '' }}" href="/admin">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li>
 
-        <li class="nav-item">
-            <a class="nav-link {{ Request::is('admin/campaigns*') ? 'active' : '' }}" href="/admin/campaigns">
-                <i class="bi bi-flag"></i>
-                <span>Campaign</span>
-            </a>
-        </li>
+    <li class="nav-item">
+      <a class="nav-link {{ Request::is('admin/campaigns*') ? 'active' : '' }}" href="/admin/campaigns">
+        <i class="bi bi-flag"></i>
+        <span>Campaign</span>
+      </a>
+    </li>
 
-        <li class="nav-item">
-            <a class="nav-link {{ Request::is('admin/payments*') ? 'active' : '' }}" href="/admin/payments">
-                <i class="bi bi-credit-card"></i>
-                <span>Metode Pembayaran</span>
-            </a>
-        </li>
-
-    </ul>
+  </ul>
 </aside>
